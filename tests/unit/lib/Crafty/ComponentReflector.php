5a3d1e2762d498dd731819a55f0bca860eb29e24
kao98 <kao98@kaonet-fr.net>
2014-04-04 16:22:51 +0200

    Conversion of ComponentReflector unit tests from simpletest to atoum

---
+++ tests/unit/lib/Crafty/ComponentReflector.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace tests\unit;
+
+require_once('../../lib/Crafty/ComponentReflector.php');
+require_once(__DIR__ . '/_fakes/HybridInjectionClass.php');
+
+use atoum;
+
+class Crafty_ComponentReflector extends atoum {
+
+    public function testReflectorIsReflectionClass() {
+        
+        $this
+            ->object(new \Crafty_ComponentReflector(
+                    'HybridInjectionClass',
+                    array('prop3' => 'def')
+                )
+            )
+            
+                ->isInstanceOf('ReflectionClass')
+        ;
+        
+    }
+
+    public function testNewsInstanceType() {
+        
+        $reflector = new \Crafty_ComponentReflector(
+            'HybridInjectionClass',
+            array('prop3' => 'def')
+        );
+        
+        $this
+            ->object($reflector->newInstance(''))
+                ->isInstanceOf('HybridInjectionClass')
+        ;
+        
+    }
+    
+    public function testNewsInstanceArgs() {
+        
+        $reflector = new \Crafty_ComponentReflector(
+            'HybridInjectionClass',
+            array('prop3' => 'def')
+        );
+        
+        $this
+            ->object($o = $reflector->newInstance('abc'))
+            
+            ->variable($o->getProp3())
+                ->isIdenticalTo('def')
+            
+            ->variable($o->getProp1())
+                ->isIdenticalTo('abc')
+            
+            ->object($o = $reflector->newInstance('xyz', 123))
+            
+            ->variable($o->getProp3())
+                ->isIdenticalTo('def')
+            
+            ->variable($o->getProp1())
+                ->isIdenticalTo('xyz')
+            
+            ->variable($o->getProp2())
+                ->isIdenticalTo(123)
+            
+        ;
+        
+    }
+    
+}
