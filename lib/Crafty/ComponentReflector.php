6a6e269fa654477efcc77aba1094df87f3157867
Kao ..98 <kao98@kaonet-fr.net>
2014-04-05 11:17:13 +0200

    Fixing ComponentReflector ReflectionClass inheritance issue

--- lib/Crafty/ComponentReflector.php
+++ lib/Crafty/ComponentReflector.php
@@ -32,7 +32,7 @@ class Crafty_ComponentReflector extends ReflectionClass
    * @param mixed $param2...
    * @return object
    */
-  public function newInstance()
+  public function newInstance($args)
   {
     return $this->newInstanceArgs(func_get_args());
   }
@@ -42,7 +42,7 @@ class Crafty_ComponentReflector extends ReflectionClass
    * @param mixed[] $args
    * @return object
    */
-  public function newInstanceArgs(array $args)
+  public function newInstanceArgs(array $args = null)
   {
     if ($this->getConstructor())
     {
